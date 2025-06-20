d237347160d8f684348cff9b4495e48c0f89d084
lucian-xinitrc <cusmirlucian@gmail.com>
2025-06-20 17:57:17 +0000

    New Commit

--- app/api/authentication.py
+++ app/api/authentication.py
@@ -12,7 +12,7 @@ class Authentication():
 		result = api_tokens.fetchone()
 
 		if result:
-			if int(result[0]) <= 2:
+			if int(result[0]) <= 3:
 				self.auth = True
 				api_tokens.execute("UPDATE tokens SET tries = tries + 1 WHERE token = %s", (token,))
 				self.conn.commit()
