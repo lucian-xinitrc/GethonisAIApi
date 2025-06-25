3f70748dcc6c57c85f9e2e3a234a2f239cd9ed5c
lucian-xinitrc <cusmirlucian@gmail.com>
2025-06-25 17:48:25 +0000

    New commit

--- app/api/authentication.py
+++ app/api/authentication.py
@@ -8,15 +8,11 @@ class Authentication():
 		self.conn = session.conn
 	def check_auth(self, token):
 		api_tokens = self.conn.cursor()
-		api_tokens.execute("SELECT tries FROM tokens WHERE token = %s", (token,))
+		api_tokens.execute("SELECT * FROM tokens WHERE token = %s", (token,))
 		result = api_tokens.fetchone()
 
 		if result:
-			api_tokens.execute("SELECT type FROM tokens WHERE token = %s", (token,))
-			permanent = api_tokens.fetchone()
-			if int(result[0]) <= 3 or permanent[0] == "PERMANENT":
-				self.auth = True
-				api_tokens.execute("UPDATE tokens SET tries = tries + 1 WHERE token = %s", (token,))
-				self.conn.commit()
+			self.auth = True
+			self.conn.commit()
 		else:
 			self.auth = False
