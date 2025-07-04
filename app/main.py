63ce46e99838cc9d43725be6984ba31a2534c1b0
lucian-xinitrc <cusmirlucian@gmail.com>
2025-07-04 09:37:19 +0000

    New commit

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
 from api import router
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
