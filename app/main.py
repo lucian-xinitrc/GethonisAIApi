6c86a5797663857dbd1c7432a2d812c9f05f7233
lucian-xinitrc <cusmirlucian@gmail.com>
2025-07-11 17:00:46 +0000

    New routes added

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
 from api import router
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173", "http://localhost:3000", "https://your-react-domain.com"],  # React app origin
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS"],
+    allow_headers=["Content-Type", "Authorization"],
+)
 app.mount("/static", StaticFiles(directory="./static"), name="static")
 app.include_router(router)
